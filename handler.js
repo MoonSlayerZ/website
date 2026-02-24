68792f7d31d63c7e95131bad5ea992760a1f1bfe
Moon <gdowna@gmail.com>
2026-02-24 13:48:22 +0000

    random update

--- handler.js
+++ handler.js
@@ -2,5 +2,4 @@ const toggleButton = document.getElementById('legendary');
 
 toggleButton.addEventListener('click', function() {
     document.body.classList.toggle('dark-mode');
-    sleep(500); 
 });
